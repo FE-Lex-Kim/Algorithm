95a055ffeabb36514b6d980a8cbce56546eca911
EOJIN KIM <kimeojin1995@gmail.com>
2021-04-02 20:24:35 +0900

    update: 미로탈출 BFS 코드 수정

--- BFS/미로 탈출.js
+++ BFS/미로 탈출.js
@@ -5,60 +5,49 @@
 // 1인 위치만 움직일 수 있다.
 // 가장 좌측최상단에서 가장 우측하단 까지 최소이동칸의 개수를 출력해야한다.
 const escapemaze = (str) => {
+  let n;
+  let m;
   let graph = [];
-  let N;
-  let M;
   for (let line of str.split("\n")) {
     if (line.split(" ").length === 2) {
-      N = +line.split(" ")[0];
-      M = +line.split(" ")[1];
+      n = +line.split(" ")[0];
+      m = +line.split(" ")[1];
       continue;
     }
-    // 그래프를 초기화해준다.
+    // 그래프를 초기화 해준다.
     graph = [...graph, line.split("")];
   }
-  // 상하좌우 이동할 방향 정의
-  let dx = [-1, 1, 0, 0];
-  let dy = [0, 0, -1, 1];
+  // 상하좌우 이동 x,y 좌표
+  const dx = [-1, +1, 0, 0];
+  const dy = [0, 0, -1, 1];
+  let queue = [];
+  queue.push([0, 0]);
 
-  const bfs = (x, y) => {
-    let queue = [];
-    queue.push([x, y]);
+  // 큐가 다 비어있을때 멈춘다.
+  while (queue.length !== 0) {
+    let [x, y] = queue.shift();
 
-    // 큐가 빌때까지 반복한다.
-    while (queue.length !== 0) {
-      [x, y] = queue.shift();
+    for (let i = 0; i < 4; i++) {
+      let nx = x + dx[i];
+      let ny = y + dy[i];
+      // 미로 찾기 공간을 벗어난 경우 무시
+      if (nx < 0 || nx >= n || ny < 0 || ny > m) {
+        continue;
+      }
 
-      // 현재 위치에서 4가지 방향으로 위치 확인
-      for (let i = 0; i < 4; i++) {
-        let nx = x + dx[i];
-        let ny = y + dy[i];
-        // 미로 찾기 공간을 벗어난 경우 무시
-        if (nx < 0 || nx >= N || ny < 0 || ny > M) {
-          continue;
-        }
-
-        // 벽인 경우 무시
-        if (graph[nx][ny] === "0") {
-          continue;
-        }
-
-        // 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-        if (graph[nx][ny] === "1") {
-          graph[nx][ny] = +graph[x][y] + 1;
-          queue.push([nx, ny]);
-        }
+      // 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+      if (graph[nx][ny] === "1") {
+        queue.push([nx, ny]);
+        graph[nx][ny] = +graph[x][y] + 1;
       }
     }
-
-    return graph[N - 1][M - 1];
-  };
-  console.log(bfs(0, 0));
+  }
+  console.log(graph[n - 1][m - 1]);
 };
 
 escapemaze(`5 6
 111111
-000001
+000111
 111111
 100000
 111111`);
