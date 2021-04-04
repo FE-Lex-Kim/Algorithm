49e980cc90f69078dc5e5ce22c76de78b0ba170b
EOJIN KIM <kimeojin1995@gmail.com>
2021-04-04 23:19:02 +0900

    update: 구름 코딩테스트 첫번째줄 제외시키기 코드 추가

--- 노트/구름 코딩테스트 템플릿.js
+++ 노트/구름 코딩테스트 템플릿.js
@@ -1,3 +1,6 @@
+// 줄 별로 line으로 값이 출력된다.
+// 1
+// 2 3 4 5
 const test = (str) => {
   for (let line of str.split("\n")) {
     console.log(line);
@@ -6,3 +9,22 @@ const test = (str) => {
 
 test(`1
 2 3 4 5`);
+
+// 만약 첫 째줄을 거르고 그 다음 줄부터 값을 사용하고 싶은 코드
+// 첫 째줄의 첫번째 요소와, 두번째 요소를 N과 M값으로 할당
+// 두 번째줄을 배열에 넣음
+// arr에 두번째줄부터 배열이 할당
+const cook = (str) => {
+  let arr = [];
+  for (let line of str.split("\n")) {
+    arr = [...arr, line.split(" ")];
+  }
+  let N = arr[0][0];
+  let M = arr[0][1];
+
+  arr.splice(0, 1);
+  console.log(arr);
+};
+
+cook(`4 6
+19 15 10 17`);
