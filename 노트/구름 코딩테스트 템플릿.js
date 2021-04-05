dbd899b5276baa1f107e0bdede167c45716f9c69
EOJIN KIM <kimeojin1995@gmail.com>
2021-04-05 18:36:06 +0900

    update: 구름 코딩테스트 / test2 함수 / 첫번째 줄 변수할당 및 두번째 줄 배열 요소 문자열에서 숫자로 변경 코드 작성

--- 노트/구름 코딩테스트 템플릿.js
+++ 노트/구름 코딩테스트 템플릿.js
@@ -1,30 +1,37 @@
 // 줄 별로 line으로 값이 출력된다.
 // 1
 // 2 3 4 5
-const test = (str) => {
+const test1 = (str) => {
   for (let line of str.split("\n")) {
     console.log(line);
   }
 };
 
-test(`1
+test1(`1
 2 3 4 5`);
 
 // 만약 첫 째줄을 거르고 그 다음 줄부터 값을 사용하고 싶은 코드
 // 첫 째줄의 첫번째 요소와, 두번째 요소를 N과 M값으로 할당
 // 두 번째줄을 배열에 넣음
 // arr에 두번째줄부터 배열이 할당
-const cook = (str) => {
-  let arr = [];
+const test2 = (str) => {
+  let testArr = [];
   for (let line of str.split("\n")) {
-    arr = [...arr, line.split(" ")];
+    testArr = [...testArr, line.split(" ")];
   }
-  let N = arr[0][0];
-  let M = arr[0][1];
 
-  arr.splice(0, 1);
+  // 첫번째 줄에서 첫번째, 두번째 요소 N, M에 할당
+  let N = +testArr[0][0];
+  let M = +testArr[0][1];
+
+  // 첫번째줄 배열에서 삭제
+  testArr.splice(0, 1);
+
+  // 문자열을 숫자로 바꾼다.
+  let arr = testArr[0].map((todo) => +todo);
+
   console.log(arr);
 };
 
-cook(`4 6
+test2(`4 6
 19 15 10 17`);
