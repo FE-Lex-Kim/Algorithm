dc5a9f1b52ab56ea0f8a3f98343fee9d3c26e0a7
EOJIN KIM <kimeojin1995@gmail.com>
2021-04-13 00:09:34 +0900

    create: 행렬의 덧셈

---
+++ 프로그래머스 연습문제/행렬의 덧셈.js
@@ -0,0 +1,22 @@
+function solution(arr1, arr2) {
+  var answer = arr1;
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr1[i].length; j++) {
+      answer[i][j] = arr1[i][j] + arr2[i][j];
+    }
+  }
+
+  return answer;
+}
+
+solution(
+  [
+    [1, 2],
+    [2, 3],
+  ],
+  [
+    [3, 4],
+    [5, 6],
+  ]
+);
