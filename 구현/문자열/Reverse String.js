ac7a1ad10ad605d0858480b7929a4bd9f44d5e34
EOJIN KIM <kimeojin1995@gmail.com>
2021-04-23 09:33:51 +0900

    create: leetcode / string, two pinter / Reverse String

---
+++ 구현/문자열/Reverse String.js
@@ -0,0 +1,17 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let i = 0;
+  let j = s.length - 1;
+
+  while (i < j) {
+    [s[i], s[j]] = [s[j], s[i]];
+    i++;
+    j--;
+  }
+  console.log(s);
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
