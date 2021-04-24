5ba0f80ae85b41f4f40618dee8303b6089e67808
EOJIN KIM <kimeojin1995@gmail.com>
2021-04-24 13:37:03 +0900

    update: Check If Two String Arrays are Equivalent 다른 풀이

--- 구현/문자열/Check If Two String Arrays are Equivalent.js
+++ 구현/문자열/Check If Two String Arrays are Equivalent.js
@@ -10,4 +10,11 @@ var arrayStringsAreEqual = function (word1, word2) {
   return str1 === str2;
 };
 
+var arrayStringsAreEqual = function (word1, word2) {
+  let str1 = word1.join("");
+  let str2 = word2.join("");
+
+  return str1 === str2;
+};
+
 arrayStringsAreEqual(["ab", "c"], ["a", "bc"]);
