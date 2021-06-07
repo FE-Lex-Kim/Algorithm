aa33309f43c7c7417b0d14d3e0970296d068e2fb
EOJIN KIM <kimeojin1995@gmail.com>
2021-06-08 04:01:54 +0900

    update: 노트 / 다익스트라 최단거리 알고리즘

--- 노트/다익스트라 최단거리 알고리즘.js
+++ 노트/다익스트라 최단거리 알고리즘.js
@@ -1,73 +1,159 @@
-function solution(N, road) {
-  // 변수 준비물 3가지
+function solution(N, road, K) {
+  let answer = 0;
+  // 변수
+  let queqe = [];
   let graph = Array.from({ length: N + 1 }, () => []);
-  let visited = Array.from({ length: N + 1 }, () => false);
   let distance = Array.from({ length: N + 1 }, () => Number.MAX_SAFE_INTEGER);
-  distance[0] = "dont'see";
 
-  // 그래프 초기화
-  for (let i = 0; i < road.length; i++) {
-    const element = road[i];
-    graph[element[0]].push([element[1], element[2]]);
-    graph[element[1]].push([element[0], element[2]]);
+  // 초기화 작업
+  // 1. 그래프 정보 초기화
+  for (const ele of road) {
+    let a = ele[0];
+    let b = ele[1];
+    let c = ele[2];
+    graph[a].push([b, c]);
+    graph[b].push([a, c]);
   }
-  console.log(graph);
 
-  // 함수 준비물 1가지
-  // 방문하지 않은 노드중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
-  const getSmallestNode = () => {
-    let min = Number.MAX_SAFE_INTEGER;
-    let index = 0;
-    for (let i = 1; i < N + 1; i++) {
-      const element = visited[i];
-      if (!element && distance[i] < min) {
-        min = distance[i];
-        index = i;
-      }
-    }
+  const dijkstra = (startNode) => {
+    // 2. 시작노드 최단거리 초기화
+    distance[0] = "Never mind";
+    distance[startNode] = 0;
 
-    return index;
-  };
+    // 3. 큐 안에 시작노드 초기화
+    queqe.push(startNode);
 
-  // 다익스트라 시작 노드를 매개변수값으로 넣는다.
-  const dijkstra = (start) => {
-    // 시작노드 초기화
-    visited[start] = true;
-    distance[start] = 0;
+    while (queqe.length) {
+      let node = queqe.shift();
 
-    // startNode의 인접 노드의 최단거리 값(distance)을 갱신
-    for (let i = 0; i < graph[start].length; i++) {
-      const element = graph[start][i];
-      distance[element[0]] = element[1];
-    }
-
-    // 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
-    for (let i = 0; i < N - 1; i++) {
-      // 방문하지 않은 노드중 가장 짧은 거리를 가진 노드를 현재 노드로 선정
-      let now = getSmallestNode();
-      // 방문한 노드로 갱신
-      visited[now] = true;
-      // 현재 노드에서 인접노드중 최단거리가 있는지 확인후 갱신
-      for (let i = 0; i < graph[now].length; i++) {
-        const element = graph[now][i];
-        // 현재 노드에서 다음 노드로 가는 거리
-        const cost = distance[now] + element[1];
-        // 다음노드의 최단거리 와 현재노드에서 다음노드로 가는 거리 를 비교
-        if (cost < distance[element[0]]) {
-          distance[element[0]] = cost;
+      for (const [adjNode, adjTime] of graph[node]) {
+        if (distance[node] + adjTime < distance[adjNode]) {
+          distance[adjNode] = distance[node] + adjTime;
+          queqe.push(adjNode);
         }
       }
     }
   };
 
   dijkstra(1);
+
+  for (const time of distance) {
+    if (time <= K) answer++;
+  }
+
+  return answer;
 }
 
-solution(5, [
-  [1, 2, 1],
-  [2, 3, 3],
-  [5, 2, 2],
-  [1, 4, 2],
-  [5, 3, 1],
-  [5, 4, 2],
-]);
+// solution(
+//   5,
+//   [
+//     [1, 2, 1],
+//     [2, 3, 3],
+//     [5, 2, 2],
+//     [1, 4, 2],
+//     [5, 3, 1],
+//     [5, 4, 2],
+//   ],
+//   3
+// );
+
+/* 
+가장 작은 최단거리 노드부터 방문하는 이유는
+가장 작은 노드를 갱신한 즉시 해당 노드의 인접노드의 최단거리도 자동으로 최단거리로 갱신되어 질것이다.
+이때 최단거리가 가장 작은 노드를 먼저 꺼내서 이 작업을 해주면 그렇지 않았을때와 다르게 가장 작을 때부터 작업을 하므로
+먼저 인접노드들이 가장 최단거리가 짧은 값을 기준으로 갱신되기에 그 인접노드들도 짧게 갱신된다.
+
+만약 그 반대로 아예 최단거리가 갱신된 노드들 중에 가장 최단거리가 긴 노드부터 방문을 한다면
+그 인접노드들 갱신한다면 만약 이후에 최단거리가 더 짧은 노드의 인접한 노드와 공통된다라고 하면 또다시 갱신하는 작업을 거쳐야하므로
+가장 짧은 노드를 먼저 하는게 효율성이 높다.
+*/
+
+// 우선순위큐를 사용한 다익스트라 최단거리 알고리즘
+function solution2(N, road, K) {
+  // 기준을 요소의 1번째인덱스로 오름차순으로 정렬했다.
+  // 만약 인덱스의 위치에 따라 오름차순을 수정하고 싶으면 하단의 클래스의 1 부분을 수정하면된다.
+  // 만약 오름차순이거나, 객체로 해당값을 정렬 하고싶으면 for문부터 수정해야한다.
+  class PriorityQueue {
+    constructor() {
+      this.priorityQueue = [];
+      this.length = 0;
+    }
+
+    push(newItem) {
+      this.length++;
+      let isAdded = false;
+      for (let i = 0; i < this.priorityQueue.length; i++) {
+        const element = this.priorityQueue[i];
+        if (newItem[1] < element[1]) {
+          this.priorityQueue.splice(i, 0, newItem);
+          isAdded = true;
+          break;
+        }
+      }
+
+      if (!isAdded) this.priorityQueue.push(newItem);
+    }
+
+    shift() {
+      this.length--;
+      return this.priorityQueue.shift();
+    }
+  }
+
+  let answer = 0;
+  // 변수
+  let priorityQueue = new PriorityQueue();
+  let graph = Array.from({ length: N + 1 }, () => []);
+  let distance = Array.from({ length: N + 1 }, () => Number.MAX_SAFE_INTEGER);
+
+  // 초기화 작업
+  // 1. 그래프 정보 초기화
+  for (const ele of road) {
+    let a = ele[0];
+    let b = ele[1];
+    let c = ele[2];
+    graph[a].push([b, c]);
+    graph[b].push([a, c]);
+  }
+
+  const dijkstra = (startNode) => {
+    // 2. 시작노드 최단거리 초기화
+    distance[0] = "Never mind";
+    distance[startNode] = 0;
+
+    // 3. 큐 안에 시작노드 초기화
+    priorityQueue.push([startNode, 0]);
+
+    while (priorityQueue.length) {
+      let [node, curNodeTime] = priorityQueue.shift();
+
+      for (const [adjNode, adjTime] of graph[node]) {
+        if (curNodeTime + adjTime < distance[adjNode]) {
+          distance[adjNode] = curNodeTime + adjTime;
+          priorityQueue.push([adjNode, distance[adjNode]]);
+        }
+      }
+    }
+  };
+
+  dijkstra(1);
+
+  for (const time of distance) {
+    if (time <= K) answer++;
+  }
+
+  return answer;
+}
+
+solution2(
+  5,
+  [
+    [1, 2, 1],
+    [2, 3, 3],
+    [5, 2, 2],
+    [1, 4, 2],
+    [5, 3, 1],
+    [5, 4, 2],
+  ],
+  3
+);
