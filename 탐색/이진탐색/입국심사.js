d027021fe6432f219cc33317f75dba8b6fe42dcf
EOJIN KIM <kimeojin1995@gmail.com>
2021-07-17 11:05:32 +0900

    update: 입국심사 복습에대한 오답노트

--- 탐색/이진탐색/입국심사.js
+++ 탐색/이진탐색/입국심사.js
@@ -9,7 +9,7 @@ function solution(n, times) {
       0
     );
 
-    if (possibleEnter <= n) {
+    if (possibleEnter >= n) {
       end = mid;
     } else if (possibleEnter < n) {
       start = mid + 1;
@@ -20,3 +20,12 @@ function solution(n, times) {
 }
 
 solution(6, [7, 10]);
+
+/* 
+오답노트 : 
+1. 정답을 도출하는 로직을 생각하지 못함.
+2. 직접 하나하나 구현하는 코드가 길어지는 프로그래밍이라면 코테에서 나올 확률이 적으므로,
+    정답이 나오는 다른 로직이 있다고 고려해보기
+3. 이진탐색에서 mid만을 target에 비교하는 것이아니라, 
+    mid값을 추가적으로 가공해서 나온 값을 target과 비교해서 나올 수 있음을 몰랐음
+*/
