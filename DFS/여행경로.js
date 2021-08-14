2c840349b1a0c31171fe212551a592c849d6d4eb
EOJIN KIM <kimeojin1995@gmail.com>
2021-08-14 21:06:56 +0900

    update: 여행경로 순수함수로 dfs 코드 수정

--- DFS/여행경로.js
+++ DFS/여행경로.js
@@ -19,8 +19,12 @@ function solution(tickets) {
   };
 
   dfs("ICN", ["ICN"], 0);
+  console.log(answer);
   return answer.sort()[0];
 }
+
+// [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
+// [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
 
 solution([
   ["ICN", "SFO"],
@@ -30,5 +34,38 @@ solution([
   ["ATL", "SFO"],
 ]);
 
-// [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
-// [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
+function solution2(tickets) {
+  let answer = [];
+
+  let graph = {};
+  let visited = Array.from({ length: tickets.length }, () => false);
+
+  for (let i = 0; i < tickets.length; i++) {
+    const [from, to] = tickets[i];
+    graph[from] ? graph[from].push(i) : (graph[from] = [i]);
+  }
+
+  const dfs = (from, count, route, total) => {
+    if (count === tickets.length) {
+      return total.push(route);
+    }
+
+    if (!graph[from]) return;
+
+    for (const n of graph[from]) {
+      // 방문하지 않은 간선이라면 방문한다.
+      if (!visited[n]) {
+        visited[n] = true;
+        // 다음에 방문할 노드
+        dfs(tickets[n][1], count + 1, [...route, tickets[n][1]], total);
+
+        visited[n] = false;
+      }
+    }
+
+    return total;
+  };
+
+  answer = dfs("ICN", 0, ["ICN"], []).sort()[0];
+  return answer;
+}
