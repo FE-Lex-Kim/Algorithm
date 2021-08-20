ecdaa9a15a7d7a15fb62a16650448bc7d17ddfb8
EOJIN KIM <kimeojin1995@gmail.com>
2021-08-21 01:27:10 +0900

    update: 순열과 조합

--- 노트/순열과 조합.js
+++ 노트/순열과 조합.js
@@ -3,12 +3,17 @@ const getCombinations = function (arr, selectNumber) {
   const results = [];
   if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
 
+  // 재귀를 생각할때에는 마지막 부분과 그 바로 윗부분 생각해서 이어붙일지 로직을 생각하면 쉽다.
   arr.forEach((fixed, index, origin) => {
     const rest = origin.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
     const combinations = getCombinations(rest, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
     const attached = combinations.map((combination) => [fixed, ...combination]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
     results.push(...attached); // 배열 spread syntax 로 모두다 push
   });
+
+  // 재귀의 (마지막에 반환된 값) 과 그 중간에 반환된 값이 같은 타입으로 반환되어야 한다.
+  // 마지막에 반환된 값 = [[0],[1]]
+  // 중간에 반환된 값 = [[1,2], [2,3]]
 
   return results; // 결과 담긴 results return
 };
