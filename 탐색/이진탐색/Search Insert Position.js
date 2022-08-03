a75b6cf704eb4917839ba00fb83f15aca5e2b50c
EOJIN KIM <kimeojin1995@gmail.com>
2022-08-03 19:47:26 +0900

    update: 탐색 / 이진 탐색 / Search Inser Position / leatCode

--- 탐색/이진탐색/Search Insert Position.js
+++ 탐색/이진탐색/Search Insert Position.js
@@ -4,26 +4,26 @@
  * @return {number}
  */
 var searchInsert = function (nums, target) {
-  if (target < nums[0]) {
-    return 0;
-  } else if (nums[nums.length - 1] < target) {
-    return nums.length;
-  }
-
   let left = 0;
   let right = nums.length - 1;
-  let mid;
+
   while (left < right) {
-    mid = left + Math.floor((right - left) / 2);
+    let mid = Math.ceil((left + right) / 2);
+
     if (nums[mid] === target) {
       return mid;
-    } else if (target < nums[mid]) {
-      right = mid;
+    } else if (nums[mid] < target) {
+      left = mid;
     } else {
-      left = mid + 1;
+      right = mid - 1;
     }
   }
 
-  return left;
+  if (nums[left] < target) {
+    return left + 1;
+  } else {
+    return left;
+  }
 };
-console.log(searchInsert([1, 3, 5, 6], 4));
+
+searchInsert([1, 3, 5, 6], 2);
